ac67c7e6bab556578b3e6c0761c2697d8b921f8a
jungminji <wjdalswl0215@gmail.com>
2024-07-16 21:01:40 +0900

    카테고리 클릭시 해당 영역 이동

---
+++ team.js
@@ -0,0 +1,13 @@
+// TODO 임시파일입니다.
+
+document.getElementById("category-1").addEventListener("click", function () {
+  document.getElementById("team-desc").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("category-2").addEventListener("click", function () {
+  document.getElementById("member-desc").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("category-3").addEventListener("click", function () {
+  document.getElementById("gallery").scrollIntoView({ behavior: "smooth" });
+});
