dd0b674891dac4f54ec640c7fec4dcd943703914
789elf <789elf@naver.com>
2024-07-18 16:32:41 +0900

    수정기능 추가 (상위 방명록만 우선 적용)

--- scripts/guest_book.js
+++ scripts/guest_book.js
@@ -1,12 +1,15 @@
 // Firebase SDK 라이브러리 가져오기
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase, set, update, ref } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 import {
     getFirestore,
     doc,
     getDocs,
+    getDoc,
     collection,
     addDoc,
     updateDoc,
+    writeBatch,
     where,
     setDoc,
     deleteDoc,
@@ -50,7 +53,7 @@ $("#addEntry").click(async function () {
     let guest_pw = $("#guest-pw").val();
     let guest_message = $("#guest-message").val();
     let date = getDate();
-    
+
     isRoot = true;
 
     var obj = {
@@ -74,7 +77,7 @@ $(document).on("click", "#addReply", async function () {
     let guest_pw = $("#reply-guest-pw").val();
     let guest_message = $("#reply-guest-message").val();
     let date = getDate();
-    
+
     isRoot = false;
 
     var obj = {
@@ -97,7 +100,7 @@ $(document).on("click", "#addReply", async function () {
 async function saveDoc(obj) {
     const docRef = await addDoc(collection(db, "guest_book"), obj);
 
-    if(isRoot){
+    if (isRoot) {
         updateRootDoc(docRef, obj);
     }
 
@@ -107,7 +110,7 @@ async function saveDoc(obj) {
 }
 
 // 방명록일경우 본인의 id를 parentId로 업데이트 (parentId = doc.id)
-async function updateRootDoc(docRef, obj){
+async function updateRootDoc(docRef, obj) {
     obj.parentId = docRef.id;
     await setDoc(docRef, obj)
 }
@@ -128,7 +131,7 @@ $(window).load(async () => {
 async function loadGuestBook() {
     let docs = await getDocs(
         query(
-            collection(db, "guest_book"), 
+            collection(db, "guest_book"),
             orderBy("isRoot", "desc"),
             orderBy("date", "desc")
         )
@@ -147,44 +150,128 @@ var delRow;
 // 삭제버튼 클릭 이벤트 (클릭된 방명록의 db id값 가져옴)
 $(document).on("click", ".deleteBtn", async function () {
     fieldId = $(this).attr("id");
+    mode = "delete"
 });
 
-// 모달 삭제버튼 클릭 이벤트
+
+function deleteProcess() {
+    if (delRow.isRoot) {
+        multiDeleteDocs().then(() => {
+            clearAll();
+        })
+    } else {
+        singleDeleteDoc().then(() => {
+            clearAll();
+        })
+    }
+}
+
+// 방명록 삭제 -> 하위 답글 전부 삭제
+async function multiDeleteDocs() {
+    const q = query(collection(db, "guest_book"), where("parentId", "==", delRow.parentId));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+        deleteDoc(doc.ref);
+    });
+}
+
+// 모달 확인 버튼 클릭 이벤트
 $('#validPassword').click(async function () {
     const inputPw = $('#inputPw').val();
     let docs = await getDocs(collection(db, "guest_book"), doc);
 
     if (checkPassword(inputPw, docs, fieldId)) {
-
-        if(delRow.isRoot){
-            multiDeleteDocs();
-        }else{ 
-            singleDeleteDoc();
+        if (mode == "delete") {
+            deleteProcess()
         }
-
-        cardClear();
-        loadGuestBook();
-        closeModal();
+        else if (mode == "update") {
+            createUpdateForm();
+        }
     } else {
         closeModal();
         window.alert('비밀번호가 틀렸습니다.');
     }
 })
 
-// 방명록 삭제 -> 하위 답글 전부 삭제
-async function multiDeleteDocs(){
-    const q = query(collection(db, "guest_book"), where("parentId", "==", delRow.parentId));
-    const querySnapshot = await getDocs(q);
-    
-    querySnapshot.forEach((doc) => {
-        deleteDoc(doc.ref);
-    });
+// 클리어, 다시 로드
+function clearAll() {
+    cardClear();
+    loadGuestBook();
+    closeModal();
 }
 
+
+
 // 답글 삭제 -> 답글 1개만 삭제
-async function singleDeleteDoc(){
+async function singleDeleteDoc() {
     await deleteDoc(doc(db, "guest_book", fieldId));
 }
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * 방명록, 답글 수정 * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+let mode;
+
+$(document).on("click", ".updateBtn", async function () {
+    mode = "update";
+    fieldId = $(this).attr("id");
+});
+
+
+// 답글 폼 생성
+async function createUpdateForm() {
+    closeModal();
+
+    const tempDiv = document.getElementById('container-' + fieldId);
+
+    const updateGuestName = tempDiv.querySelector('.show-name').textContent;
+    const updateGuestMessage = tempDiv.querySelector('.show-guest-message').textContent;
+
+
+    let updateInput = $(`
+        <div class="input-container">
+            <div class="input-name-pw-container">
+                <input type="text" class="input-name" id="update-guest-name" value="${updateGuestName}"/>
+                <div class="input-submit-container">
+                    <button id="updateEntry">수정</button>
+                    <button id="updateCancle">취소</button>
+                  </div>
+            </div>
+            <div class="input-message-container">
+                <textarea type="text" class="input-message" id="update-guest-message">${updateGuestMessage}</textarea>
+            </div>
+        </div>
+    `)
+
+    tempDiv.insertAdjacentElement('afterend', updateInput[0]);
+    $(tempDiv).hide();
+
+    // 취소 버튼 (나중에 이벤트 따로 만들어야함)
+    // $(updateInput).hide();
+}
+
+// 수정 폼 -> 수정 버튼 클릭 이벤트 (db값 수정)
+$(document).on("click", "#updateEntry", async function () {
+
+    const updateGuestName = $('#update-guest-name').val();
+    const updateGuestMessage = $('#update-guest-message').val()
+
+    const docRef = doc(collection(db, "guest_book"), fieldId);
+    const docSnap = await getDoc(docRef);
+
+    let row = docSnap.data();
+    row.guest_name = updateGuestName;
+    row.guest_message = updateGuestMessage;
+
+    await setDoc(docRef, row);
+
+    clearAll();
+});
+
+
+
 
 
 
@@ -216,15 +303,17 @@ function createGuestBook(docs, name) {
                 <div class="show-container" id="container-${id}">
                     <div class="show-name-container">
                         <span class="show-name">${guest_name}</span>
-                        <span class="show-guest-massage">${guest_message}</span> 
+                        <span class="show-guest-message">${guest_message}</span> 
                         <span class="show-date">${date}</span>
                     </div>
                     <div class="show-button-container">
-                        <button class="deleteBtn" data-bs-toggle="modal" id=${id} data-bs-target="#passwordModal">삭제</button>
                         <button class="replyBtn" id="${id}">답글</button>
+                        <button class="deleteBtn" data-bs-toggle="modal" id=${id} data-bs-target="#passwordModal">삭제</button>
+                        <button class="updateBtn" data-bs-toggle="modal" id=${id} data-bs-target="#passwordModal">수정</button>
                     </div>
                 </div>
             `);
+            // <button class="updateBtn" id="${id}">수정</button>
 
             $("#guestbook-entries").append(guest_book);
 
@@ -243,10 +332,11 @@ function createGuestBook(docs, name) {
                     </div>
                     <div class="reply-show-message-container">
                         <button class="deleteBtn" data-bs-toggle="modal" id=${id} data-bs-target="#passwordModal">삭제</button>
+                        <button class="updateBtn" data-bs-toggle="modal" id=${id} data-bs-target="#passwordModal">수정</button>
                     </div>
                 </div>
             `)
-            
+
             parentDiv.insertAdjacentElement('afterend', guest_book[0]);
         }
 
