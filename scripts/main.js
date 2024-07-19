555569ce6d2d3a3f58dfde934be902aac3f6c49c
임보라 <imbora@imbolaui-MacBookAir.local>
2024-07-19 10:43:02 +0900

    슬라이드 드래그 추가

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const galleryWrapper = document.querySelector(".gallery-wrapper"); //갤러리 랩퍼
     const galleryItem = document.querySelectorAll(".gallery-item"); //갤러리 아이템
     let slider = document.querySelector(".gallery-container");
-    let isDown = false; //클릭 상태 체크
     let startX; //마우스 드래그 시작점 x좌표 체크
-    let scrollLeft; //마우스 드래그시 x좌표 체크
-    const paginationContainer = document.querySelector(
-      ".gallery-pagination"
-    ); //페이지네이션 부모
+    let endX; //마우스 드래그시 x좌표 체크
+    const paginationContainer = document.querySelector(".gallery-pagination"); //페이지네이션 부모
     const nextBtn = document.querySelector(".next-btn"); //다음 버튼
     const prevBtn = document.querySelector(".prev-btn"); //이전 버튼
-    let btn;
 
+    // 페이지네이션 엑션
     const handleButtonClick = (i) => {
       document.querySelectorAll(".gallery-btn").forEach((button) => {
         button.classList.remove("active");
@@ -22,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
       galleryWrapper.style.transform = `translateX(-${i * (100 / 6)}%)`;
     };
 
+    // 페이지네이션 버튼 생성 후 엑션
     for (let i = 0; i < galleryItem.length; i++) {
       const btn = document.createElement("span");
       btn.className = "gallery-btn";
@@ -33,31 +31,33 @@ document.addEventListener("DOMContentLoaded", () => {
       paginationContainer.appendChild(btn);
     }
 
+    // 다음 엑션
+    function nextMove(){
+      const activeIndex = [...document.querySelectorAll(".gallery-btn"),].findIndex((button) => button.classList.contains("active"));
+      const nextIndex = (activeIndex + 1) % galleryItem.length;
+      handleButtonClick(nextIndex);
+      prevBtn.classList.remove("hightOn");
+      nextBtn.classList.add("hightOn");
+    }
       nextBtn.addEventListener("click", () => {
-        const activeIndex = [
-          ...document.querySelectorAll(".gallery-btn"),
-        ].findIndex((button) => button.classList.contains("active"));
-        const nextIndex = (activeIndex + 1) % galleryItem.length;
-        handleButtonClick(nextIndex);
-        prevBtn.classList.remove("hightOn");
-        nextBtn.classList.add("hightOn");
+        nextMove()
       });
 
+    // 이전 엑션
+    function prevMove(){
+      const activeIndex = [...document.querySelectorAll(".gallery-btn"),].findIndex((button) => button.classList.contains("active"));
+      const prevIndex =
+        (activeIndex - 1 + galleryItem.length) % galleryItem.length;
+      handleButtonClick(prevIndex);
+      nextBtn.classList.remove("hightOn");
+      prevBtn.classList.add("hightOn");
+    }
       prevBtn.addEventListener("click", () => {
-        const activeIndex = [
-          ...document.querySelectorAll(".gallery-btn"),
-        ].findIndex((button) => button.classList.contains("active"));
-        const prevIndex =
-          (activeIndex - 1 + galleryItem.length) % galleryItem.length;
-        handleButtonClick(prevIndex);
-        nextBtn.classList.remove("hightOn");
-        prevBtn.classList.add("hightOn");
+        prevMove()
       });
 
     /* 갤러리 이미지 호버시 크기조정 */
-    const galleryImgs = document.querySelectorAll(
-      ".gallery-item ul li img"
-    );
+    const galleryImgs = document.querySelectorAll(".gallery-item ul li img");
     galleryImgs.forEach((img) => {
       img.addEventListener("mouseover", () => {
         img.classList.add("on");
@@ -68,7 +68,26 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
+    /*갤러리 드래그*/ 
+    slider.addEventListener("mousedown", (e) => {
+      console.log("mousedown", e.pageX);
+      startX = e.pageX; // 마우스 드래그 시작 위치 저장
+      // galleryItem.classList.add("grab");
+    });
 
+    slider.addEventListener("mouseup", (e) => {
+      console.log("mouseup", e.pageX);
+      endX = e.pageX; // 마우스 드래그 끝 위치 저장
+      if (startX < endX) {
+        // 마우스가 오른쪽으로 드래그 된 경우
+        console.log("prev move");
+        prevMove();
+      } else if (startX > endX) {
+        // 마우스가 왼쪽으로 드래그 된 경우
+        console.log("next move");
+        nextMove();
+      }
+    });
 
 
 
